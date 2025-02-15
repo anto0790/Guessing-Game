9995c08c6eb71929ff0c3846928ff398062838b1
Antonela <antod0790@gmail.com>
2025-02-15 11:54:18 -0300

    The play guess

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let numberRandom=Math.floor(Math.random()*10)+1;
+
+let entryNumber=document.getElementById('entryNumber');
+let message=document.getElementById('message');
+
+function checkResult(){
+    let number= parseInt(entryNumber.value);
+
+    if(number < 1 || number > 10 || isNaN(number)){
+        message.textContent = "The number entered is incorrect";
+        message.style.color = 'black';
+        return;
+    }
+
+    if(number === numberRandom){
+        message.textContent = "The number is correct";
+        message.style.color = 'green';
+        entryNumber.disabled = true;
+    }
+    else if(number < numberRandom){
+    message.textContent = "The number is lower";
+    message.style.color = 'red';
+    }
+    else{
+        message.textContent = "The number is largest";
+        message.style.color = 'red';
+    }
+    
+}
